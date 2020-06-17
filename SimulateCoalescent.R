eb4f4495a59b035884d1b08b9a4b91a30efff8fa
David <David.Helekal@warwick.ac.uk>
2020-06-17 16:09:34 +0100

    Update SimulateCoalescent.R

--- SimulateCoalescent.R
+++ SimulateCoalescent.R
@@ -29,32 +29,40 @@ simulate <- function(sampling_times, pop_size){
                         t <- times_desc[idx]
                 } else {
                         #Pick waiting time
+                        rate <- choose(extant_lineages,2)/pop_size*extant_lineages
+                        
                         if (idx+1 > length(sampling_times)) {
                                 delta_t <- Inf
                         } else {
                                 delta_t <- t-times_desc[idx+1]
                         }
-                        rate <- choose(extant_lineages,2)/pop_size*extant_lineages
-                        w_t <- (-1.0/rate)*log(1-runif(n=1, min = 0, max= 1))
-                        print(delta_t)
-                        if (w_t>delta_t){
-                                #if waiting time longer than interval
-                                #move to next sampling event, restart
-                                idx <- idx+1
-                                extant_lineages <- extant_lineages+1
-                                future_lineages <- future_lineages-1
-                                t <- times_desc[idx]
-                        } else {
-                                #if waiting time within interval, coalesce
+                        
+                        p_coal <- 1-exp(-rate*delta_t)
+                        r <- runif(1,0,1)
+                        
+                        if  (r < p_coal) {
+                                r_w <- runif(1,0,1)
+                                w_t <- inv_t_conditional(r_w,rate, delta_t)
+                                
+                                lh <- lh * p_coal * cond_likelihood(rate, r_w, delta_t)
+                                
                                 t <- t + w_t
                                 extant_lineages <- extant_lineages-1 
                                 coalescent_times[coal_idx] <- t
                                 coal_idx <- coal_idx+1
+                        } else {
+                                lh <- lh * (1-p_coal)
+                                idx <- idx+1
+                                extant_lineages <- extant_lineages+1
+                                future_lineages <- future_lineages-1
+                                t <- times_desc[idx]
                         }
                 }
         }
         ### add return likelihood
-        return(coalescent_times)
+        coal_sample<-list(coalescent_times=coalescent_times, likelihood=lh)
+
+        return(coal_sample)
 }
 
 build_coal_tree <- function(sampling_times, coalescent_times){
