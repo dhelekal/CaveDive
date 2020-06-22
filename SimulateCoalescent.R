4ca114af583b3b05b91c5ab3d56d3ad8dbf23b67
David <David.Helekal@warwick.ac.uk>
2020-06-22 13:45:42 +0100

    Add conditional likelihood based sampling, add coalescent time log-likelihood, add + fix tree plots

--- SimulateCoalescent.R
+++ SimulateCoalescent.R
@@ -10,7 +10,7 @@ cond_likelihood <- function(rate, u, delta_t){
 
 simulate <- function(sampling_times, pop_size){
         
-        lh <- 1 
+        log_lh <- 0
                 
         times_desc <- sampling_times[order(-sampling_times)]
         future_lineages <- length(sampling_times)-1
@@ -40,18 +40,18 @@ simulate <- function(sampling_times, pop_size){
                         p_coal <- 1-exp(-rate*delta_t)
                         r <- runif(1,0,1)
                         
-                        if  (r < p_coal) {
+                        if  (r <= p_coal) {
                                 r_w <- runif(1,0,1)
                                 w_t <- inv_t_conditional(r_w,rate, delta_t)
                                 
-                                lh <- lh * p_coal * cond_likelihood(rate, r_w, delta_t)
+                                log_lh <- log_lh + log(p_coal) + log(cond_likelihood(rate, r_w, delta_t))
                                 
-                                t <- t + w_t
+                                t <- t - w_t
                                 extant_lineages <- extant_lineages-1 
                                 coalescent_times[coal_idx] <- t
                                 coal_idx <- coal_idx+1
                         } else {
-                                lh <- lh * (1-p_coal)
+                                log_lh <- log_lh + log((1-p_coal))
                                 idx <- idx+1
                                 extant_lineages <- extant_lineages+1
                                 future_lineages <- future_lineages-1
@@ -59,12 +59,59 @@ simulate <- function(sampling_times, pop_size){
                         }
                 }
         }
+        
         ### add return likelihood
-        coal_sample<-list(coalescent_times=coalescent_times, likelihood=lh)
+        coal_sample<-list(coalescent_times=coalescent_times,
+                          log_likelihood=log_lh)
 
         return(coal_sample)
 }
 
 build_coal_tree <- function(sampling_times, coalescent_times){
+        coal_times_desc <- coalescent_times[order(-coalescent_times)]
+        times_desc <- sampling_times[order(-sampling_times)]
+        tree_nodes <- seq(1,length(sampling_times))
+        tree_nodes <- sapply(tree_nodes, function (x) return (paste0("S", x)))
+        extant_entries <- c(1)
+        extant_times <- c(times_desc[1])
+
+        coal_idx <- 1 
+        s_idx <- 1
+        t <- times_desc[1]
         
+        while (coal_idx <= length(coalescent_times)) {
+                if (s_idx<length(times_desc) && (times_desc[s_idx+1] > coal_times_desc[coal_idx])){
+                        s_idx <- s_idx+1  
+                        t <- times_desc[s_idx]
+                        extant_entries <- c(extant_entries, s_idx)
+                        extant_times <- c(extant_times, t)
+                } else {
+                        t <- coal_times_desc[coal_idx]
+                        coal_node1_idx <- trunc(runif(1,1,length(extant_entries)+1))
+                        coal_node1 <- extant_entries[coal_node1_idx]
+                        extant_entries <- extant_entries[-coal_node1_idx]
+                        ct1 <- extant_times[coal_node1_idx]
+                        extant_times <- extant_times[-coal_node1_idx]
+                        br_len_1 <- ct1-t
+                        entry1<-tree_nodes[coal_node1]
+                        
+                        coal_node2_idx <- trunc(runif(1,1,length(extant_entries)+1))
+                        coal_node2 <- extant_entries[coal_node2_idx]
+                        ct2 <- extant_times[coal_node2_idx]
+                        br_len_2 <- ct2-t
+                        entry2<-tree_nodes[coal_node2]
+                        
+                        tree_nodes[coal_node2] <- paste0("(",
+                                                         entry1,
+                                                         ":",br_len_1,
+                                                         ",",
+                                                         entry2,
+                                                         ":",br_len_2,
+                                                         ")")
+                        extant_times[coal_node2] <- t
+                        coal_idx <- coal_idx+1
+                }
+        }
+        tree_str<-paste0(tree_nodes[extant_entries[1]], ";")
+        return(tree_str)
 }
