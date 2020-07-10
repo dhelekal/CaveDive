6b5aa1ec0b1f674f028a8a747a734992ccac127b
David <David.Helekal@warwick.ac.uk>
2020-07-10 17:16:36 +0100

    use improper prior for N

--- R/run_mcmc_exponential.R
+++ R/run_mcmc_exponential.R
@@ -11,7 +11,7 @@ sam <- sam[order(-sam)]
 gt.N = runif(1, 1, 100)
 gt.lambda = runif(1, 0.1, 10)
 
-x0 <- c(runif(1,-30, 30), runif(1,0.1,1000))
+x0 <- c(runif(1,-30, 30), runif(1,0.1,400))
 
 pdf(file="tree.pdf")
 co <- plot_exp_growth(sam, gt.lambda, gt.N)
@@ -26,8 +26,8 @@ log_lh <- function(x){
 
   if (n > 0){
     prior_lambda <- dlaplace(lambda, m=0, s=1, log=TRUE)
-    prior_n <- dexp(n, rate = 1, log = TRUE)
-    lh <- exponential_coalescent_loglh(sam, times, lambda, n) + prior_n + prior_lambda
+    prior_n <- 0 #dexp(n, rate = 1, log = TRUE)
+    lh <- exponential_coalescent_loglh(sam, times, lambda, n) + prior_lambda + prior_n
   } else {
     lh <- -Inf
   }
@@ -38,12 +38,12 @@ prop.sampler <-function (x_prev){
   return(c(rnorm(1, mean=x_prev[1], 1), rnorm(1, mean=x_prev[2], 1)))
 }
 
-proposal.cond_lh <-function(x_prev, x_cand){
+proposal.cond_lh <-function(x_cand, x_prev){
   return(log(dnorm(x_cand[1],x_prev[1],1)) + log(dnorm(x_cand[2],x_prev[2],1)))
 }
 
-n_it <- 6000000
-burn_in <-1000000
+n_it <- 1000000
+burn_in <-30000
 
 o <- run_mcmc(log_lh, proposal.cond_lh, prop.sampler, x0, n_it)
 
