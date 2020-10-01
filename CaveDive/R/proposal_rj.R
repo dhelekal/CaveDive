612d603718fec803d4ee2cd8d3db8c819205cf61
David <David.Helekal@warwick.ac.uk>
2020-10-01 16:56:15 +0100

    add rjmcmc proposal file

---
+++ CaveDive/R/proposal_rj.R
@@ -0,0 +1,213 @@
+###list of: N, div_event_1, ... div_event_N
+###div_event list of r, K, x_0, branch
+
+prop.sampler <- function (x_prev, i_prev, pre, para.initialiser){
+  p <- 1 
+  r <- runif(1,0,2) 
+
+  if (r < p) { ## transdimensional
+    upd <- transdimensional.sampler(x_prev, i_prev, pre)
+    x_next <- upd$x_next
+    i_next <- upd$i_next
+  } else { ## within-model
+    x_next <- within_model.sampler(x_prev, i_prev, pre)
+    i_next <- i_prev
+  }
+  return(list(x_next=x_next,i_next=i_next))
+}
+
+transdimensional.sampler <- function(x_prev, i_prev, pre, para.initialiser) {
+  which_move <- sample.int(2,size=1)
+  if (which_move==1) { ### increase dim
+    x_next <- x_prev
+    i_next <- i_prev + 1
+    append(x_next, para.initialiser(pre))
+  } else { ### decrease dim
+    x_next <- x_prev
+    which_elem <- sample.int(i_prev, size=1)
+    x_next <- x_next[[-which_elem]]
+    i_next <- i_prev - 1
+  }
+  return(list(x_next=x_next, i_next=i_next))
+}
+
+within_model.sampler <- function(x_prev, i_prev, pre) {
+
+  which_move <- sample.int(4, size=1)
+
+  if (which_move==1) {
+    x_next <- make_move(x_prev, i_prev, pre, move_update_rates)
+  } else if(which_move==2) {
+    x_next <- make_move(x_prev, i_prev, pre, move_update_time)
+  } else if(which_move==3) {
+    x_next <- make_move(x_prev, i_prev, pre, move_update_branch)
+  } else if(which_move==4) {
+    N_prev <- x_prev[[1]]
+    N_next <- move_update_N(N_prev, pre)
+
+    x_next <- x_prev
+    x_next[[1]] <- N_next 
+  }
+  return(x_next)
+}
+
+make_move <- function(x_prev, i_prev, pre, move) {
+    which_model <- 1+sample.int(i_prev, size=1)
+    mdl_prev <- x_prev[[which_model]] 
+    mdl_next <- move(mdl_prev, pre)
+    x_next <- x_prev
+    x_next[[which_model]] <- mdl_next
+    return(x_next)
+}
+
+prop.cond_log_lh <- function(x, given, pre) {
+
+  rates_given <- given[[1]]
+  K_given <- given[[2]]
+  N_given <- given[[3]]
+  div.times_given <- given[[4]]
+  div.branch_given <- given[[5]]
+
+  rates <- x[[1]]
+  K <- x[[2]]
+  N <- x[[3]]
+  div.times <- x[[4]]
+  div.branch <- x[[5]]
+
+  outgoing <- pre$outgoing
+  edges <- pre$edges.df
+
+  param_log_lh <- sum(dnorm(rates, mean=rates_given, sd=1, log=TRUE)) + 
+         sum(dnorm(K, mean=K_given, sd=1, log=TRUE)) +
+         sum(dnorm(N, mean=N_given, sd=1, log=TRUE))
+
+  time_log_lh <- 0
+  branch_log_lh <- 0
+  for (i in c(1:length(div.branch))) {
+    if (div.branch[i] == div.branch_given[i]) {
+      time_log_lh <- time_log_lh + dnorm(div.times[i],
+                                          mean=div.times_given[i],
+                                          sd=1)#sd=max(edges$length[div.branch[i]], 1))
+    } else {
+      branch_log_lh <- log(1/2)
+      if (!all(outgoing[[edges$node.child[div.branch_given[i]]]] != div.branch[i])) {
+        branch_log_lh <- branch_log_lh + log(1/2)
+      }
+    }
+  }
+  return(param_log_lh+time_log_lh+branch_log_lh)
+}
+
+move_update_rates` <- function(x_prev, pre) { ### update rates
+  
+  x_next <- vector(mode = "list", length = length(x_prev))
+
+  rates <- x_prev[[1]]
+  K <- x_prev[[2]]
+  N <- x_prev[[3]]
+  div.times <- x_prev[[4]]
+  div.branch <- x_prev[[5]]
+
+  which_idx <- sample.int(length(rates), size=1)
+
+  K_upd <- K
+  rates_upd <- rates
+
+  rates_upd[which_idx] <- rnorm(1, mean=rates[which_idx], sd=1) 
+  K_upd[which_idx]  <- rnorm(1, mean=K[which_idx], sd=1)
+
+  x_next[[1]] <- rates_upd 
+  x_next[[2]] <- K_upd
+  x_next[[3]] <- N
+  x_next[[4]] <- div.times
+  x_next[[5]] <- div.branch
+
+  return(x_next)
+}
+
+move_update_time <- function(x_prev, pre) { ### update time
+  
+  x_next <- vector(mode = "list", length = length(x_prev))
+
+  rates <- x_prev[[1]]
+  K <- x_prev[[2]]
+  N <- x_prev[[3]]
+  div.times <- x_prev[[4]]
+  div.branch <- x_prev[[5]]
+
+  which_idx <- sample.int(length(div.times), size=1)
+  div.times_upd <- div.times
+  div.times_upd <- rnorm(1, mean=div.times[which_idx], sd=1)#sd=max(pre$edges.df$length[div.branch[which_idx]]))
+
+  x_next[[1]] <- rates
+  x_next[[2]] <- K
+  x_next[[3]] <- N
+  x_next[[4]] <- div.times_upd
+  x_next[[5]] <- div.branch
+
+  return(x_next)
+}
+
+move_update_branch <- function(x_prev, pre) { ### update branch
+  x_next <- vector(mode = "list", length = length(x_prev))
+
+  rates <- x_prev[[1]]
+  K <- x_prev[[2]]
+  N <- x_prev[[3]]
+  div.times <- x_prev[[4]]
+  div.branch <- x_prev[[5]]
+
+  edges <- pre$edges.df
+  nodes <- pre$nodes.df
+
+  outgoing <- pre$outgoing
+  incoming <- pre$incoming
+
+  root <- pre$root_idx
+  which_half <- pre$which_half
+
+  div.branch_upd <- div.branch
+  div.times_upd <- div.times
+
+  which_idx <- sample.int(length(div.branch), size=1)
+  ## decide whether moving up or down
+  r1 <- runif(1,1,3)
+
+  if (r1 < 2) { ###up
+    if (edges$node.parent[div.branch[which_idx]]!=root) {
+        div.branch_upd[which_idx] <- incoming[[edges$node.parent[div.branch[which_idx]]]]
+      } else {
+        div.branch_upd[which_idx] <- outgoing[[root]][which(outgoing[[root]]!=div.branch[which_idx])]
+      }
+  } else { ###down
+    r2 <- runif(1,1,3)
+
+    if (r2 < 2) {
+        div.branch_upd[which_idx] <- outgoing[[edges$node.child[div.branch[which_idx]]]][1]
+    } else {
+        div.branch_upd[which_idx] <- outgoing[[edges$node.child[div.branch[which_idx]]]][2]  
+    }
+  }
+
+  old_len <- edges$length[div.branch[which_idx]]
+  new_len <- edges$length[div.branch_upd[which_idx]]
+
+  rel_pos <- (div.times[which_idx]-nodes$times[edges$node.parent[div.branch[which_idx]]])/old_len
+  div.times_upd[which_idx] <- nodes$times[edges$node.parent[div.branch_upd[which_idx]]] + new_len*rel_pos
+  
+  x_next[[1]] <- rates
+  x_next[[2]] <- K
+  x_next[[3]] <- N
+  x_next[[4]] <- div.times_upd
+  x_next[[5]] <- div.branch_upd
+
+  return(x_next)
+}
+
+
+move_update_N <- function(N_prev, pre) {
+
+  N_upd <- rnorm(1, mean = N_prev, sd = 1)
+  return(N_upd)
+
+} 
---
+++ CaveDive/R/rjmcmc.R
@@ -0,0 +1,37 @@
+rjmcmc <- function(likelihood, proposal.cond_log_lh, proposal.sampler, prior_i, x0, i0. max_it, thinning=1) {
+
+    pb <- utils::txtProgressBar(min=0, max=max_it, style = 3)
+    out.para <- vector('list',max_it/thinning)
+    out.i <- vector('list',max_it/thinning)
+
+    x_prev <- x0
+    i_prev <- i0
+    prev_lh <- likelihood(x0)+prior_i(i0)
+
+    for(it in c(1:max_it)) {
+        if (it%%thinning == 0) {
+            utils::setTxtProgressBar(pb, it)
+            out.para[[it/thinning]] <- x_prev 
+            out.i[[it/thinning]] <- i_prev
+        } 
+
+        prop <- proposal.sampler(x_prev, i_prev)
+        x_prop <- prop$x
+        i_prop <- prop$i 
+
+        prop_lh <- likelihood(x_prop) + prior_i(i0)
+
+        if (prop_lh > -Inf){
+            a <- proposal.cond_log_lh(x_prev, i_prev, x_prop, i_prop)+prop_lh
+            b <- proposal.cond_log_lh(x_cand, i_prop, x_prev, i_prev)+prev_lh
+            alpha <- min(a-b, 0)
+
+            r <- log(runif(1))
+            if (alpha < r) {
+                x_prev <- x_prop
+                i_prev <- i_prop
+            }
+        }
+    }
+    return(list(para=out.para, dims=out.i))
+}
