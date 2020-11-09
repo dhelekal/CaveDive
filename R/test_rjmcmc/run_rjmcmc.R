4224e712688fe64e96d3ef092444f57fe1b01b3b
David <David.Helekal@warwick.ac.uk>
2020-11-09 12:31:02 +0000

    rewrite mcmc test to match

--- R/test_rjmcmc/run_rjmcmc.R
+++ R/test_rjmcmc/run_rjmcmc.R
@@ -8,32 +8,32 @@ library(viridis)
 
 set.seed(12345678)
 
-n <- 2
+poi_rate <- 3
+concentration <- 3
 
 n_tips <- 100
-sam <- runif(n_tips, 0, 0.1)
+sam <- runif(n_tips, 0, 10)
 sam <- sam - max(sam)
 sam <- sam[order(-sam)]
 
-colours <- trunc(runif(n_tips, 1, n+2))
+r_mean <- 1
+r_sd <- 1
 
-N <- 10#rexp(1, rate = 1/100)
-K <- rep(N, n)#rexp(n, rate = 1/100)
-A <- rlnorm(n, meanlog = 0, sdlog = 0.25) 
-div_times <- c(-1*runif(n,1,3), -Inf)
-div_times <- div_times[order(-div_times)]
+K_mean <- 5
+K_sd <- 0.5
 
-div_cols <- c(1:(n+1))
+time_shape <- 30
+time_rate <- 5**(-1)
 
-rates <- lapply(c(1:n), function(i) return(function (s) sat.rate(s, K[i], A[i], div_times[i])))
-rates[[n+1]] <- function (s) constant.rate(s, N)
+sim <- outbreaks_simulate(poi_rate, concentration, sampling_times, r_mean, r_sd, K_mean, K_sd, time_rate, time_shape)
 
-rate.ints <- lapply(c(1:n), function(i) return(function (t, s) sat.rate.int(t, s, K[i], A[i], div_times[i])))
-rate.ints[[n+1]] <- function(t, s) constant.rate.int(t, s, N)
+co <- sim$co
+tip_cols <- sim$tip_colours
+div_times <- sim$div_times
+div_cols <- sim$div_cols
 
-co <- structured_coal.simulate(sam, colours, div_times, div_cols, rates, rate.ints)
 
-tree.div.str <- build_coal_tree.structured(sam, co$times, colours, co$colours, div_times, div_cols, co$div_from, include_div_nodes = TRUE)
+tree.div.str <- build_coal_tree.structured(sam, co$times, tip_cols, co$colours, div_times, div_cols, co$div_from, include_div_nodes = TRUE)
 tree.div <- read.tree(text = tree.div.str$full)
 tree.plt <- plot_structured_tree(tree.div, n+1)
 
@@ -41,24 +41,30 @@ pdf(file="tree_structured.pdf")
 plot(tree.plt)
 dev.off()
 
-tree.str <- build_coal_tree.structured(sam, co$times, colours, co$colours, div_times, div_cols, co$div_from, include_div_nodes = FALSE, aux_root = FALSE)
+tree.str <- build_coal_tree.structured(sam, co$times, tip_cols, co$colours, div_times, div_cols, co$div_from, include_div_nodes = FALSE, aux_root = FALSE)
 tree <- read.tree(text = tree.str$full)
 pre <- structured_coal.preprocess_phylo(tree)
 
-prior_i <- function(x) if(x < 3) log(1/3) else log(0) # dpois(x, 1, log = TRUE)
+prior_i <- function(x) dpois(x, 3, log = TRUE)
 
-prior_N <- function(x) dlnorm(x, meanlog = 1, sdlog = 1, log = TRUE)
-prior_N.sample <- function() rlnorm(1, meanlog = 1, sdlog = 1) 
+prior_N <- function(x) dlnorm(x, meanlog = K_mean, sdlog = K_sd, log = TRUE)
+prior_N.sample <- function() rlnorm(1, meanlog = K_mean, sdlog = K_sd) 
 
-prior_r <- function(x) dlnorm(x, meanlog = 0, sdlog = 0.25, log = TRUE) 
-prior_r.sample <- function(x) rlnorm(1, meanlog = 0, sdlog = 0.25) 
+prior_r <- function(x) dlnorm(x, meanlog = r_mean, sdlog = r_sd, log = TRUE) 
+prior_r.sample <- function(x) rlnorm(1, meanlog = r_mean, sdlog = r_sd) 
 
-prior_K <- function(x) dlnorm(x, meanlog = 1, sdlog = 1, log = TRUE)
-prior_K.sample <- function() rlnorm(1, meanlog = 1, sdlog = 1) 
+prior_K <- function(x) dlnorm(x, meanlog = K_mean, sdlog = K_sd, log = TRUE)
+prior_K.sample <- function() rlnorm(1, meanlog = K_mean, sdlog = K_sd) 
+
+prior_t <- function(x) dgamma(x + min(sam), shape=time_shape, scale = 1/time_rate)
+prior_t.sample <-function() min(sam)-rgamma(1, shape=time_shape, scale = 1/time_rate)
+
 
 set.seed(0)
 
-o <- infer_outbreaks(tree, prior_i, prior_N, prior_N.sample, prior_r, prior_r.sample, prior_K, prior_K.sample, n_it=1e6, thinning=1, debug=F)
+o <- outbreaks_infer(tree, prior_i,  prior_N,  prior_N.sample, 
+                     prior_r, prior_r.sample,  prior_K,  prior_K.sample,  prior_t,
+                     prior_t.sample, concentration, n_it=1e6, thinning=1, debug=TRUE)
 
 y <- sapply(o$dims, function(x) x)
 n <- sapply(o$para, function(x) x[[1]])
