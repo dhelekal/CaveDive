5c8df1c03445fa5f04b62f4a9805fbfa151f25b0
David <David.Helekal@warwick.ac.uk>
2021-02-19 14:20:47 +0000

    better inference interface

--- CaveDive/R/expansions_infer.R
+++ CaveDive/R/expansions_infer.R
@@ -1,22 +1,106 @@
 offset <- 2
 
+
+#' Generates a list of priors of the form described in the paper to use with MCMC inference. 
+#' @param expansion_rate corresponds to '\phi' in paper 
+#' @param N_mean_log corresponds to '\mu_{anc}' in paper 
+#' @param N_sd_log corresponds to '\sigma_{anc}' in paper 
+#' @param t_mid_rate corresponds to '\lambda_r' in paper 
+#' @param K_sd_log corresponds to '\sigma_{exp}' in paper 
+#' @param exp_time_nu corresponds to '\nu' in paper 
+#' @param exp_time_kappa corresponds to '\kappa' in paper 
+#' @returns a list of prior likelihoods and sampling functions with names 'prior_i', 'prior_N', 'prior_t_mid_given_N', 'prior_t_mid_given_N.sample', 'prior_K_given_N', 'prior_K_given_N.sample', 'prior_t_given_N', 'prior_t_given_N.sample'. See `standard_priors` for details.
 #' @export
-expansions_infer <- function(phy,
+#' 
+standard_priors <- function(expansion_rate=1, 
+                            N_mean_log=4, 
+                            N_sd_log=4, 
+                            t_mid_rate=10, 
+                            K_sd_log=1/2, 
+                            exp_time_nu=1/2, 
+                            exp_time_kappa=1/10) {
+    return(list(
+        prior_i=function(x) dpois(x, expansion_rate, log = TRUE),
+        prior_N=function(x) dlnorm(x, meanlog = N_mean_log, sdlog = N_sd_log, log = TRUE),
+        prior_t_mid_given_N=function(x, N) dexp(x, t_mid_rate/N, log = TRUE),
+        prior_t_mid_given_N.sample=function(N) rexp(1, t_mid_rate/N),
+        prior_K_given_N=function(x, N) dlnorm(x, meanlog = log(N), sdlog = K_sd_log, log = TRUE),
+        prior_K_given_N.sample=function(N) rlnorm(1, meanlog = log(N), sdlog = K_sd_log),
+        prior_t_given_N=function(x, N) {
+               if (all(x < 0)) {
+                      out <- dgamma(-x, shape=(exp_time_nu^2)/exp_time_kappa, scale = exp_time_kappa * N / exp_time_nu, log = TRUE)
+               } else {
+                      out <- -Inf
+               }
+               return(out) ### Uniform time prior
+        },
+        prior_t_given_N.sample=function(N) (-rgamma(1, shape=(exp_time_nu^2)/exp_time_kappa, scale = exp_time_kappa * N / exp_time_nu))
+        ))
+}
+
+
+#' Run rjmcmc inference on the provided phylogeny and supplied priors
+#' This is the standard method for mcmc expansion inference.
+#' @param phy phylogeny to run inference on 
+#' @param priors a list of prior likelihoods and sampling functions with names 'prior_i', 'prior_N', 'prior_t_mid_given_N', 'prior_t_mid_given_N.sample', 'prior_K_given_N', 'prior_K_given_N.sample', 'prior_t_given_N', 'prior_t_given_N.sample'. See `standard_priors` for details.
+#' @param concentration concentration parameter for the dirichlet prior on expansion membership probabilities
+#' @param n_it number of MCMC iterations 
+#' @param thinning mcmc output thinning 
+#' @param init initial state for the mcmc chain 
+#' @return list with names 'phylo_preprocessed', the preprocessed phylogeny, and 'mcmc_out' a list of MCMC states
+#' @export
+#' 
+run_expansion_inference <- function(phy, priors, concentration=1, thinning=1, n_it=1e6, init=NULL) {
+    pre <- structured_coal.preprocess_phylo(phy)
+    o <- expansions_infer(pre, 
+                            priors$prior_i, 
+                            priors$prior_N, 
+                            priors$prior_t_mid_given_N, 
+                            priors$prior_t_mid_given_N.sample, 
+                            priors$prior_K_given_N, 
+                            priors$prior_K_given_N.sample, 
+                            priors$prior_t_given_N,
+                            priors$prior_t_given_N.sample,
+                            concentration,
+                            n_it=n_it,
+                            thinning=thinning,
+                            init=init)
+    return(list(phylo_preprocessed=pre, mcmc_out=o))
+}   
+
+#' Run rjmcmc inference on the provided preprocessed phylogeny, and using provided prior distributions
+#' This is a barebones method for advanced usage. For standard usage refer to `run_expansion_inference`
+#' @param pre Preprocessed phylogeny
+#' @param prior_i Prior likelihood on number of expansions
+#' @param prior_N Prior likelihood on background population size
+#' @param prior_t_mid_given_N Prior likelihood on expansion time to midpoint given background population size
+#' @param prior_t_mid_given_N.sample Sampling function for prior on time to midpoint given background population size
+#' @param prior_K_mid_given_N Prior likelihood on expansion carrying capacity given background population size
+#' @param prior_K_mid_given_N.sample Sampling function for prior on expansion carrying capacity given background population size
+#' @param prior_t_given_N Prior likelihood on expansion time given background population size
+#' @param prior_t_given_N.sample Sampling function for prior on expansion timegiven background population size
+#' @param concentration concentration parameter for the dirichlet prior on expansion membership probabilities
+#' @param n_it number of MCMC iterations 
+#' @param thinning mcmc output thinning 
+#' @param init initial state for the mcmc chain 
+#' @param debug debug flag
+#' @return a list of MCMC states
+#' @export
+#' 
+expansions_infer <- function(pre,
                             prior_i, 
                             prior_N, 
-                            prior_N.sample, 
-                            prior_tmid_given_N, 
-                            prior_tmid_given_N.sample, 
+                            prior_t_mid_given_N, 
+                            prior_t_mid_given_N.sample, 
                             prior_K_given_N, 
                             prior_K_given_N.sample, 
                             prior_t_given_N,
                             prior_t_given_N.sample,
                             concentration,
-                            n_it=1e6, thinning=1, init=NULL,  debug=FALSE) {
+                            n_it=1e6, thinning=1, init=NULL, debug=FALSE) {
 
     if (debug) warning("Running in debug mode with only priors in use.")
 
-    pre <- structured_coal.preprocess_phylo(phy)
     edges <- pre$edges.df
     nodes <- pre$nodes.df
     n_tips <- pre$n_tips
@@ -68,7 +152,7 @@ expansions_infer <- function(phy,
                                          i,
                                          prior_i, 
                                          prior_N,
-                                         prior_tmid_given_N, 
+                                         prior_t_mid_given_N, 
                                          prior_K_given_N, 
                                          prior_t_given_N, 
                                          prior_probs,
@@ -77,13 +161,13 @@ expansions_infer <- function(phy,
                                                       i_prev, 
                                                       pre, 
                                                       function(N) para.initialiser(N, 
-                                                                                   prior_tmid_given_N.sample,
+                                                                                   prior_t_mid_given_N.sample,
                                                                                    prior_K_given_N.sample, 
                                                                                    prior_t_given_N.sample,
                                                                                    pre),
                                                       function(x_init, N) para.log_lh(x_init,
                                                                                       N,
-                                                                                      prior_tmid_given_N, 
+                                                                                      prior_t_mid_given_N, 
                                                                                       prior_K_given_N, 
                                                                                       prior_t_given_N,
                                                                                       pre),
@@ -103,12 +187,12 @@ fn_log_J_inv <- function(i_prev, x_prev, x_next, which_mdl_rm) {
     return(0)
 }
 
-para.initialiser <- function(N, prior_tmid_given_N, prior_K_given_N, prior_t_given_N, pre){
+para.initialiser <- function(N, prior_t_mid_given_N, prior_K_given_N, prior_t_given_N, pre){
     edges <- pre$edges.df
     nodes <- pre$nodes.df
     x_next <- vector(mode = "list", length = 4)
 
-    mid.times <- prior_tmid_given_N(N)
+    mid.times <- prior_t_mid_given_N(N)
     K <- prior_K_given_N(N)
     div.times <- prior_t_given_N(N)
 
@@ -133,7 +217,7 @@ para.initialiser <- function(N, prior_tmid_given_N, prior_K_given_N, prior_t_giv
     return(x_next)
 }
 
-para.log_lh <- function(x, N, prior_tmid_given_N, prior_K_given_N, prior_t_given_N, pre) {
+para.log_lh <- function(x, N, prior_t_mid_given_N, prior_K_given_N, prior_t_given_N, pre) {
     edges <- pre$edges.df
     nodes <- pre$nodes.df
 
@@ -150,7 +234,7 @@ para.log_lh <- function(x, N, prior_tmid_given_N, prior_K_given_N, prior_t_given
     extant_inner <- br_extant_after[which(edges$node.child[br_extant_after]>pre$n_tips)] 
     if (length(extant_inner) < 1) extant_inner <- c(NA)
 
-    out <- prior_tmid_given_N(mid.times,N) + prior_K_given_N(K,N) + prior_t_given_N(div.times,N) + log(1/length(extant_inner))
+    out <- prior_t_mid_given_N(mid.times,N) + prior_K_given_N(K,N) + prior_t_given_N(div.times,N) + log(1/length(extant_inner))
     return(out)
 }
 
@@ -158,7 +242,7 @@ log_posterior <- function(x,
                             i, 
                             prior_i, 
                             prior_N, 
-                            prior_tmid_given_N, 
+                            prior_t_mid_given_N, 
                             prior_K_given_N, 
                             prior_t_given_N, 
                             prior_probs, 
@@ -213,7 +297,7 @@ log_posterior <- function(x,
                  lgamma(i+1) ### Correction for exchangeable RVs
             if (i > 0) {
                     prior <- prior + 
-                             sum(prior_tmid_given_N(mid.times, N)) +
+                             sum(prior_t_mid_given_N(mid.times, N)) +
                              sum(prior_K_given_N(K,N)) + 
                              sum(prior_t_given_N(div.times,N)) -
                              lgamma(length(div.times)) ### prior on divergence events
