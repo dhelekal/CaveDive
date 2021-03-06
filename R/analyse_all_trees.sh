34c11cb69fb9b2baa0cbf7f2f5ab5b5f8d65709d
David <David.Helekal@warwick.ac.uk>
2021-03-06 13:53:59 +0000

    fix typo

--- R/analyse_all_trees.sh
+++ R/analyse_all_trees.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ls ./trees | parallel -j 20 --verbose --halon-error 2 eval sh "run_analyse_tree.sh ./trees/{}"
+ls ./trees | parallel -j 20 --verbose --halt-on-error 2 eval sh "run_analyse_tree.sh ./trees/{}"
